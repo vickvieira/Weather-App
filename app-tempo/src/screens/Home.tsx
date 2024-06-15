87058f2740ec8a276dba34ee422199219057ec10
Victoria <vkvieira@yahoo.com>
2024-06-15 18:54:18 -0300

    sparkles: feat: funcionalidade de utilizar a localização do dispositivo do usuário para fazer o fetch do tempo local

--- app-tempo/src/screens/Home.tsx
+++ app-tempo/src/screens/Home.tsx
@@ -2,8 +2,11 @@ import { View, Text, Button, ActivityIndicator, StyleSheet} from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { DrawerTypes } from '../routes/Drawer';
+import * as Location from 'expo-location';
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=-25.4219574&lon=-49.2664899&appid=a4b36c4a3a87e7ab769ae13ab4b529b9&units=metric`;
+
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
+const OPEN_WEATHER_KEY = `a4b36c4a3a87e7ab769ae13ab4b529b9`;
 
 type Weather = {
   name: string;
@@ -20,39 +23,60 @@ type Weather = {
 };
 
 const Home = () => {
+  const navigation = useNavigation<DrawerTypes>();
+  const [weather, setWeather] = useState();
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMsg, setErrorMsg] = useState("");
 
-    const navigation = useNavigation<DrawerTypes>();
-
-    const [weather, setWeather] = useState()
-
-    const fetchWeather = async () =>  {
-        console.log("Fetch data");
-        const results = await fetch(url);
-        const data = await results.json();
-        console.log(JSON.stringify(data, null, 2));
-        setWeather(data);
-
+  useEffect(() => {
+    //para fazer o fetch dos dados da api assim que o componente for rodado
+    if (location) {
+      fetchWeather();
     }
+  }, [location]); //faz o fetch apenas quando a localização muda
 
-    useEffect(() => { //para fazer o fetch dos dados da api assim que o componente for rodado
-        fetchWeather();
-    }, [])
+  useEffect(() => {
+    //para pegar a localização do dispositivo
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
 
-    if (!weather) {
-        return <ActivityIndicator/>;
+      let location = await Location.getCurrentPositionAsync({});
+      console.log("Location: ", location);
+      setLocation(location);
+    })();
+  }, []);
+
+  const fetchWeather = async () => {
+    if (!location){
+        return;
     }
+    console.log("Fetch data");
+    const results = await fetch(
+      `${BASE_URL}?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${OPEN_WEATHER_KEY}&units=metric`
+    );
+    const data = await results.json();
+    console.log(JSON.stringify(data, null, 2));
+    setWeather(data);
+  };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.location}>{weather.name}</Text>
-            <Text style={styles.temp}>{Math.round(weather.main.temp)}°C</Text>
-            
+  if (!weather) {
+    return <ActivityIndicator />;
+  }
 
-            {/* <Button title="Detalhes" onPress={() => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.location}>{weather.name}</Text>
+      <Text style={styles.temp}>{Math.round(weather.main.temp)}°C</Text>
+
+      {/* <Button title="Detalhes" onPress={() => {
                 navigation.navigate("Detalhes")
             }} ></Button> */}
-        </View>
-    );
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
