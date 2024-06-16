eb5b8b98fb8ece0ecb4af621b32c7313c22e86c9
Victoria <vkvieira@yahoo.com>
2024-06-16 18:01:32 -0300

    :sparkles: feat: implementação da SearchBar na pg Home

---
+++ app-tempo/src/components/SearchBar.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet, TouchableOpacity, Text, Pressable } from "react-native";
+
+interface SearchBarProps {
+  onSearch: (city: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [city, setCity] = useState<string>("");
+
+  const handleSearch = () => { //desconsidera espaços antes e depois da busca
+    const trimmedCity = city.trim();
+    onSearch(trimmedCity);
+  };
+
+  return (
+    <View style={styles.searchContainer}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Digite o nome da cidade"
+        placeholderTextColor="gray"
+        value={city}
+        onChangeText={setCity}
+      />
+      <TouchableOpacity onPress={handleSearch}>
+        <Text style={styles.buttonText}>Buscar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 60,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    color: "white",
+    marginRight: 10,
+    width: "70%",
+  },
+  button: {
+    color: 'blue',
+  },
+  buttonText: {
+    color: 'white',
+  }
+});
+
+export default SearchBar;
+
+
+
+
